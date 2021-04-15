49f2e96bb73b39f7296405cd6a2f2727085336ca
PriscoCoppola <priscocoppola7@gmail.com>
2021-04-15 18:04:25 +0200

    fix function

--- js/main.js
+++ js/main.js
@@ -86,5 +86,5 @@ console.table(newTeams);
 
 // Functions
 function randomNumber(min, max) {
-    return Math.floor( Math.random() * (max - min + 1)) - min;
+    return Math.floor( Math.random() * (max - min)) + min;
 }
